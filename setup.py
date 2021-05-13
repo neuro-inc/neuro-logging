97d156c1c2c9ba18dfec531b56e743a996e6f5c8
Ivan Zubenko <vanya.zubenko@gmail.com>
2021-05-13 11:17:46 +0300

    update package repo url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="platform-logging",
     python_requires=">=3.7.0",
-    url="https://github.com/neuromation/platform-logging",
+    url="https://github.com/neuro-inc/platform-logging",
     packages=find_packages(),
     setup_requires=["setuptools_scm"],
     install_requires=[
