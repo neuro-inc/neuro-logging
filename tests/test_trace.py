3bce7c5aa81334db6b96b374d02e7556b078fb81
Andrew Svetlov <andrew.svetlov@gmail.com>
2025-01-03 12:14:59 +0100

    Switch to sentry 2.0 SDK design

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -20,24 +20,19 @@ from neuro_logging.trace import (
 @pytest.fixture()
 def sentry_transaction() -> t.Iterator[None]:
     sentry_sdk.init(traces_sample_rate=1.0)
-    hub = sentry_sdk.Hub.current
-    with (
-        sentry_sdk.Hub(hub) as hub,
-        hub.start_transaction(
-            Transaction(name="test", hub=hub, parent_sampled=True, sampled=True)
-        ),
+    with sentry_sdk.start_transaction(
+        Transaction(name="test", parent_sampled=True, sampled=True)
     ):
         yield
 
 
 @pytest.mark.usefixtures("sentry_transaction")
-@pytest.mark.xfail
 async def test_sentry_trace() -> None:
-    parent_span = sentry_sdk.Hub.current.scope.span
+    parent_span = sentry_sdk.get_current_scope().span
 
     @trace
     async def func() -> None:
-        span = sentry_sdk.Hub.current.scope.span
+        span = sentry_sdk.get_current_scope().span
 
         assert span
         assert parent_span != span
@@ -48,13 +43,12 @@ async def test_sentry_trace() -> None:
 
 
 @pytest.mark.usefixtures("sentry_transaction")
-@pytest.mark.xfail
 async def test_sentry_trace_cm_data() -> None:
     async with trace_cm(
         "test", tags={"test1": "val1", "test2": "val2"}, data={"data": "value"}
     ):
 
-        span = sentry_sdk.Hub.current.scope.span
+        span = sentry_sdk.get_current_scope().span
 
         assert span
         assert span._tags["test1"] == "val1"
@@ -63,14 +57,13 @@ async def test_sentry_trace_cm_data() -> None:
 
 
 @pytest.mark.usefixtures("sentry_transaction")
-@pytest.mark.xfail
 async def test_sentry_trace_multiple_tasks() -> None:
     spans = []
 
     @trace
     async def func() -> None:
         await asyncio.sleep(0)
-        span = sentry_sdk.Hub.current.scope.span
+        span = sentry_sdk.get_current_scope().span
 
         assert span
 
@@ -83,11 +76,10 @@ async def test_sentry_trace_multiple_tasks() -> None:
     assert span1.span_id != span2.span_id
 
 
-@pytest.mark.xfail
 async def test_sentry_new_trace() -> None:
     @new_trace
     async def func() -> None:
-        span = sentry_sdk.Hub.current.scope.span
+        span = sentry_sdk.get_isolation_scope().span
 
         assert isinstance(span, Transaction)
         assert span.name == "test_sentry_new_trace.<locals>.func"
@@ -97,7 +89,6 @@ async def test_sentry_new_trace() -> None:
     await func()
 
 
-@pytest.mark.xfail
 async def test_sentry_new_trace_multiple_tasks() -> None:
     sentry_sdk.init(traces_sample_rate=1.0)
     spans: list[t.Optional[Span]] = []
@@ -105,7 +96,7 @@ async def test_sentry_new_trace_multiple_tasks() -> None:
     @new_trace
     async def func() -> None:
         await asyncio.sleep(0)
-        span = sentry_sdk.Hub.current.scope.span
+        span = sentry_sdk.get_isolation_scope().span
 
         spans.append(span)
 
@@ -118,11 +109,10 @@ async def test_sentry_new_trace_multiple_tasks() -> None:
     assert span1.trace_id != span2.trace_id
 
 
-@pytest.mark.xfail
 async def test_sentry_new_sampled_trace() -> None:
     @new_sampled_trace
     async def func() -> None:
-        span = sentry_sdk.Hub.current.scope.span
+        span = sentry_sdk.get_isolation_scope().span
 
         assert isinstance(span, Transaction)
         assert span.name == "test_sentry_new_sampled_trace.<locals>.func"
@@ -137,9 +127,11 @@ async def test_sentry_new_sampled_trace() -> None:
 async def test_sentry_notrace() -> None:
     @notrace
     async def func() -> None:
-        assert not sentry_sdk.Hub.current.scope.transaction.sampled
+        scope = sentry_sdk.get_current_scope()
+        assert not scope.transaction.sampled
 
-    assert sentry_sdk.Hub.current.scope.transaction.sampled
+    scope = sentry_sdk.get_current_scope()
+    assert scope.transaction.sampled
 
     await func()
 
